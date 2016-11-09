66a1ad927abc0b3772646d70f14d2e303c06bd65
Shav <shavenchew@yahoo.com>
2016-11-09 13:33:55 -0800

    'Testing Mongoose connection with DB'

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    shortId = require('shortid'),
+    validUrl = require('valid-url'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+//connect to Database
+mongoose.connect('mongodb://localhost/url-shortener');
+
+var linkSchema = new mongoose.Schema({
+  url : String
+});
+
+//make a collection called links in our url-shortener DB
+var Links = mongoose.model('links', linkSchema);
+
+
+//get route with link parameter
+app.get('/new/:url(*)', function(req, res ,next){
+  var url = req.params.url;
+  Links.create({
+    url : url
+  }, function(err, result){
+    if(err){
+      console.log(err);
+    } else {
+      console.log('Successful Add');
+    }
+  });
+  res.send(url);
+});
+
+
+app.listen(3000, function(){
+  console.log('Listening on 3000');
+});
